b74e4a9f23b7712891b75809da8aee1284f2d2fa
Hugo1030 <yzhjsw520@gmail.com>
2017-12-08 10:55:22 +0800

    padding sentence

--- nn-classfy.py
+++ nn-classfy.py
@@ -4,3 +4,49 @@ from collections import Counter
 import jieba
 import time
 
+
+# load file and get data
+def load_data(file):
+    line_no = 0
+    limits = 5
+    with open(file) as f:
+        data = []
+        labels = []
+        for line in f.readlines():
+            line_no += 1
+            if line_no > limits:
+                break
+            words = []
+            for word in jieba.cut(line.strip().split('\t')[0]):
+                if word>=u'\u4e00' and word<=u'\u9fff':
+                    words.append(word)
+            data.append(words)
+            labels.append(line.strip().split('\t')[1])
+        return data,labels
+
+# filename
+train_file = "corpus/train_shuffle.txt"
+test_file = "corpus/test_shuffle.txt"
+
+train_data,train_labels = load_data(train_file)
+test_data,test_labels = load_data(test_file)
+
+# fixlenth and padding
+def pad_data(data):
+    content = []
+    fix_length = 20
+    padding = ["<Pad>"]
+
+    for sentence in data:
+        if len(sentence) < fix_length:
+            sentence.extend(padding * (fix_length - len(sentence)))
+        else:
+            sentence = sentence[0: fix_length]
+        content.append(sentence)
+    return content
+
+train_content = pad_data(train_data)
+test_content = pad_data(test_data)
+print(train_content, test_content)
+
+
